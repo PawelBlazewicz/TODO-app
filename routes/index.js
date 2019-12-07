db971d293bc565d9f5da1596175b2b617c286999
Michał <kontakt@michalswiatlowski.pl>
2019-12-07 23:31:55 +0100

    auth routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express'),
-      router  = express.Router();
+      router  = express.Router(),
+      User    = require('../models/user.model'),
+      bcrypt  = require('bcryptjs');
 
 // MAIN SITE
 router.get('/', (req, res) => {
@@ -7,13 +9,61 @@ router.get('/', (req, res) => {
 });
 
 // AUTH ROUTE
-
 router.get('/register', (req, res) => {
     res.render('register');
 });
 
+router.post('/register', (req, res) => {
+    User.find({name: req.body.username})
+        .exec()
+        .then(user => {
+            if(user.length >= 1){
+                return res.send('User already exists');
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if(err){
+                        console.log(err);
+                    } else {
+                        const newUser = new User({
+                            name: req.body.username,
+                            pass: hash
+                        });
+                        console.log(newUser);
+                        newUser
+                            .save()
+                            .then(res.redirect('/login'))
+                            .catch(err =>{
+                                consile.log(err);
+                            });
+                    }
+                });
+            }
+        })
+})
+
 router.get('/login', (req, res) => {
     res.render('login');
 })
+
+router.post('/login', (req, res) => {
+    User.find({name: req.body.username})
+        .exec()
+        .then(user => {
+            if(user.length < 1) {
+                res.send('Auth failed');
+            }
+            bcrypt.compare(req.body.password, user[0].pass, (err, result) => {
+                if(err) {
+                    res.send('Auth failed');
+                }
+                if(result) {
+                    res.send('Auth successful');
+                }
+            })
+    })
+        .catch(err =>{
+        consile.log(err);
+    });
+});
 
 module.exports = router;
