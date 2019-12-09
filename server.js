f9653f3d75a1716b1ce943966fb5422136aa4dad
PawelBlazewicz <52447280+PawelBlazewicz@users.noreply.github.com>
2019-12-09 10:20:19 +0100

    Allow Cross Domain

--- server.js
+++ server.js
@@ -1,41 +1,47 @@
-const express    = require('express'),
-      bodyParser = require('body-parser'),
-      user       = require('./routes/user.route.js'),
-      app        = express(),
-      mongoose   = require('mongoose');
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  user = require("./routes/user.route.js"),
+  app = express(),
+  mongoose = require("mongoose");
 
-      var allowCrossDomain = function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    
-        // intercept OPTIONS method
-        if ('OPTIONS' == req.method) {
-          res.send(200);
-        }
-        else {
-          next();
-        }
-    };
+const allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,PATCH,DELETE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
 
 // DB CONFIG
-var dev_db_url = 'mongodb+srv://someuser:abcd1234@cluster0-wlozt.mongodb.net/test?retryWrites=true&w=majority';
+var dev_db_url =
+  "mongodb+srv://someuser:abcd1234@cluster0-wlozt.mongodb.net/test?retryWrites=true&w=majority";
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-// APP CONFIG 
+// APP CONFIG
 app.use(allowCrossDomain);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/API/users', user);
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/API/users", user);
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 // ROUTES REQUIRING
-const indexRoutes = require('./routes/index');
+const indexRoutes = require("./routes/index");
 
 // ROUTES INCLUDING
 app.use(indexRoutes);
@@ -43,6 +49,5 @@ app.use(indexRoutes);
 // SERVER SETUP
 let port = 3041;
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
-})
-
+  console.log("Server is up and running on port numner " + port);
+});
